d0a11b65dbeedeb1fb16f5f69c50a2d708518e14
krishna chauhan <sandhya756507@gmail.com>
2023-06-14 18:33:43 +0530

    adding the .env file

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+require('dotenv').config();
 
 
 /// mongo db connection 
@@ -164,8 +165,8 @@ app.get("/contact", function(req, res){
 });
 
 
-app.set('port', process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
 
-app.listen(app.get('port'), function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
