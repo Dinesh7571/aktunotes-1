1e4fa0c726f41514fe907e6328e3bbb3700c22fe
krishna chauhan <sandhya756507@gmail.com>
2023-06-15 06:46:26 +0530

    changes the public directory by  __dirname

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static("public"));
 require('dotenv').config();
 
 
